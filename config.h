2a96a9463b1b78119b250301de89d39cf50667c0
Yuxuan Yan <yanyuxuan@cau.edu.cn>
2022-12-14 06:52:09 +0800

    customize

--- config.h
+++ config.h
@@ -1,16 +1,16 @@
-#define CMDLENGTH 45
-#define DELIMITER "  "
+#define CMDLENGTH 100
+#define DELIMITER "|"
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("sb-mail",    1800, 17),
-	BLOCK("sb-music",   0,    18),
-	BLOCK("sb-disk",    1800, 19),
-	BLOCK("sb-memory",  10,   20),
-	BLOCK("sb-loadavg", 5,    21),
-	BLOCK("sb-mic",     0,    26),
-	BLOCK("sb-record",  0,    27),
-	BLOCK("sb-volume",  0,    22),
-	BLOCK("sb-battery", 5,    23),
-	BLOCK("sb-date",    1,    24)
+	BLOCK("sb-gpu",		1,	10),
+	BLOCK("sb-cpu",		1,	9),
+	BLOCK("sb-ram",		1,	8),
+	BLOCK("sb-disk",	60,	7),	
+	BLOCK("sb-nettraffic",	1,	6),
+	BLOCK("sb-volume",	1,	5),
+	BLOCK("sb-mic",		1,	4),
+	BLOCK("sb-internet",	5,	3),
+	BLOCK("sb-battery",	5,	2),
+	BLOCK("sb-clock",	1,	1)
 };
